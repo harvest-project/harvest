8505708235b5f76f2d0b50ebc6c23f41d70be379
Harvest Project <admin@harvest-project.org>
2020-10-25 20:44:48 +0100

    Remove unused import

--- Harvest/routing.py
+++ Harvest/routing.py
@@ -1,8 +1,6 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-import plugins.maize.routing
-
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(

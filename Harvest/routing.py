78a29b7e34c301b66f3ddd200bd40dfae070a284
Harvest Project <admin@harvest-project.org>
2020-10-25 20:33:17 +0100

    Fix improperly cleaned up routing.py

--- Harvest/routing.py
+++ Harvest/routing.py
@@ -1,10 +1,11 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
+import plugins.maize.routing
+
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
-        URLRouter(
-        )
+        URLRouter([])
     ),
 })

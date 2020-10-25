c3102130bebd781f5fd091182bac7087ded2eb94
Harvest Project <admin@harvest-project.org>
2020-10-25 20:31:58 +0100

    Remove explicit maize reference in global routing.py

--- Harvest/routing.py
+++ Harvest/routing.py
@@ -1,13 +1,10 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-import plugins.maize.routing
-
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
         URLRouter(
-            plugins.maize.routing.websocket_urlpatterns
         )
     ),
 })
